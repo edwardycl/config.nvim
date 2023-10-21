b69d9444c212e965405f38fb71149c7b23772361
Youngchan Lee <youngchan1115@gmail.com>
2023-10-21 23:28:00 +0900

    Remove statuscol

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -64,27 +64,4 @@ return {
       end
     end,
   },
-  -- TODO: Statuscol
-  -- {
-  --   "luukvbaal/statuscol.nvim",
-  --   event = "BufReadPre",
-  --   config = function()
-  --     local builtin = require("statuscol.builtin")
-  --     require("statuscol").setup({
-  --       relculright = true,
-  --       segments = {
-  --         { text = { builtin.foldfunc }, click = "v:lua.ScFa" },
-  --         {
-  --           sign = { name = { "Diagnostic" }, maxwidth = 2, auto = true },
-  --           click = "v:lua.ScSa",
-  --         },
-  --         { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
-  --         {
-  --           sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, auto = true, wrap = true },
-  --           click = "v:lua.ScSa",
-  --         },
-  --       },
-  --     })
-  --   end,
-  -- },
 }
